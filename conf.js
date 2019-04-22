d7fe89e247714c6293d3c62c87672c3d47e457d4
Sanoj Antony <sanoj.antony@oracle.com>
2019-04-22 14:08:59 -0700

    More pageobjects and test suite for Data Activation

--- conf.js
+++ conf.js
@@ -6,14 +6,38 @@ exports.config = {
 
     // Spec patterns are relative to the current working directory when
     // protractor is called.
-    specs: ['dist/testsuites/login/login-suite.spec.js'],
+    specs: [
+        'dist/testsuites/login/login-suite.spec.js',
+        'dist/testsuites/dataplan/data-plan-suite.spec.js'
+    ],
 
-    SELENIUM_PROMISE_MANAGER: false,
+    SELENIUM_PROMISE_MANAGER: false, //Disable Selenium promise manager so we can use async/await
 
     // Jasmine Options
     jasmineNodeOpts: {
-        defaultTimeoutInterval:1440000
+        defaultTimeoutInterval:40000
     },
+
+    /*capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['show-fps-counter=true']
+        }
+    },*/
+
+    //To support headless, use args: [ "--headless", "--disable-gpu", "--window-size=800,600" ] with chrome
+/*    multiCapabilities: [{
+        'browserName': 'firefox',
+        'moz:firefoxOptions': {
+            'args': ['--safe-mode'] //"--headless" to use headless caps with FF
+        }
+
+    }, {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['show-fps-counter=true']
+        }
+    }],*/
 
     // Spec patterns are relative to the location of the spec file. They may
     // include glob patterns.
